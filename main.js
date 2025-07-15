d940e6d24ae107445650fc5be514f893a922aff8
Francisco <francisco.tonini@aluno.senac.br>
2025-07-14 21:38:00 -0300

    Imagem e slides modulados adicionados

---
+++ main.js
@@ -0,0 +1,33 @@
+const criarTarefa = (evento) =>{
+    evento.preventDefault();
+    const quantidade = 3;
+    let repete = quantidade;
+    const lista = document.querySelector('[data-liastaSeguradoras]');
+    while(repete > 0){
+        const valor = repete;
+        console.log(valor);
+        const slideWrap = document.createElement('div');
+        slideWrap.classList.add('swiper-slide')
+        const conteudo = `<img src="./img/${valor}.png" alt="">`;
+        slideWrap.innerHTML = conteudo;
+        lista.appendChild(slideWrap);
+        repete -= 1;
+    }
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+        centeredSlides: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+}
+
+// const novaTarefa = document.querySelector('[data-button]');
+document.addEventListener('DOMContentLoaded', criarTarefa);
+
+
+
+//Eventos dom
+// novaTarefa.addEventListener('click', criarTarefa);
